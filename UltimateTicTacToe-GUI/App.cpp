370fbb819838e43bcee6c97499c23a805ee76b5c
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-05-23 19:02:02 -0700

    style: format source files

--- UltimateTicTacToe-GUI/App.cpp
+++ UltimateTicTacToe-GUI/App.cpp
@@ -17,20 +17,17 @@ using namespace UltimateTicTacToe_GUI::implementation;
 /// Initializes the singleton application object.  This is the first line of authored code
 /// executed, and as such is the logical equivalent of main() or WinMain().
 /// </summary>
-App::App()
-{
-    InitializeComponent();
-    Suspending({ this, &App::OnSuspending });
+App::App() {
+	InitializeComponent();
+	Suspending({ this, &App::OnSuspending });
 
 #if defined _DEBUG && !defined DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
-    UnhandledException([this](IInspectable const&, UnhandledExceptionEventArgs const& e)
-    {
-        if (IsDebuggerPresent())
-        {
-            auto errorMessage = e.Message();
-            __debugbreak();
-        }
-    });
+	UnhandledException([this](IInspectable const&, UnhandledExceptionEventArgs const& e) {
+		if (IsDebuggerPresent()) {
+			auto errorMessage = e.Message();
+			__debugbreak();
+		}
+	});
 #endif
 }
 
@@ -39,61 +36,52 @@ App::App()
 /// will be used such as when the application is launched to open a specific file.
 /// </summary>
 /// <param name="e">Details about the launch request and process.</param>
-void App::OnLaunched(LaunchActivatedEventArgs const& e)
-{
-    Frame rootFrame{ nullptr };
-    auto content = Window::Current().Content();
-    if (content)
-    {
-        rootFrame = content.try_as<Frame>();
-    }
+void App::OnLaunched(LaunchActivatedEventArgs const& e) {
+	Frame rootFrame{ nullptr };
+	auto content = Window::Current().Content();
+	if (content) {
+		rootFrame = content.try_as<Frame>();
+	}
 
-    // Do not repeat app initialization when the Window already has content,
-    // just ensure that the window is active
-    if (rootFrame == nullptr)
-    {
-        // Create a Frame to act as the navigation context and associate it with
-        // a SuspensionManager key
-        rootFrame = Frame();
+	// Do not repeat app initialization when the Window already has content,
+	// just ensure that the window is active
+	if (rootFrame == nullptr) {
+		// Create a Frame to act as the navigation context and associate it with
+		// a SuspensionManager key
+		rootFrame = Frame();
 
-        rootFrame.NavigationFailed({ this, &App::OnNavigationFailed });
+		rootFrame.NavigationFailed({ this, &App::OnNavigationFailed });
 
-        if (e.PreviousExecutionState() == ApplicationExecutionState::Terminated)
-        {
-            // Restore the saved session state only when appropriate, scheduling the
-            // final launch steps after the restore is complete
-        }
+		if (e.PreviousExecutionState() == ApplicationExecutionState::Terminated) {
+			// Restore the saved session state only when appropriate, scheduling the
+			// final launch steps after the restore is complete
+		}
 
-        if (e.PrelaunchActivated() == false)
-        {
-            if (rootFrame.Content() == nullptr)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(xaml_typename<UltimateTicTacToe_GUI::MainPage>(), box_value(e.Arguments()));
-            }
-            // Place the frame in the current Window
-            Window::Current().Content(rootFrame);
-            // Ensure the current window is active
-            Window::Current().Activate();
-        }
-    }
-    else
-    {
-        if (e.PrelaunchActivated() == false)
-        {
-            if (rootFrame.Content() == nullptr)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(xaml_typename<UltimateTicTacToe_GUI::MainPage>(), box_value(e.Arguments()));
-            }
-            // Ensure the current window is active
-            Window::Current().Activate();
-        }
-    }
+		if (e.PrelaunchActivated() == false) {
+			if (rootFrame.Content() == nullptr) {
+				// When the navigation stack isn't restored navigate to the first page,
+				// configuring the new page by passing required information as a navigation
+				// parameter
+				rootFrame.Navigate(xaml_typename<UltimateTicTacToe_GUI::MainPage>(), box_value(e.Arguments()));
+			}
+			// Place the frame in the current Window
+			Window::Current().Content(rootFrame);
+			// Ensure the current window is active
+			Window::Current().Activate();
+		}
+	}
+	else {
+		if (e.PrelaunchActivated() == false) {
+			if (rootFrame.Content() == nullptr) {
+				// When the navigation stack isn't restored navigate to the first page,
+				// configuring the new page by passing required information as a navigation
+				// parameter
+				rootFrame.Navigate(xaml_typename<UltimateTicTacToe_GUI::MainPage>(), box_value(e.Arguments()));
+			}
+			// Ensure the current window is active
+			Window::Current().Activate();
+		}
+	}
 }
 
 /// <summary>
@@ -103,9 +91,8 @@ void App::OnLaunched(LaunchActivatedEventArgs const& e)
 /// </summary>
 /// <param name="sender">The source of the suspend request.</param>
 /// <param name="e">Details about the suspend request.</param>
-void App::OnSuspending([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] SuspendingEventArgs const& e)
-{
-    // Save application state and stop any background activity
+void App::OnSuspending([[maybe_unused]] IInspectable const& sender, [[maybe_unused]] SuspendingEventArgs const& e) {
+	// Save application state and stop any background activity
 }
 
 /// <summary>
@@ -113,7 +100,6 @@ void App::OnSuspending([[maybe_unused]] IInspectable const& sender, [[maybe_unus
 /// </summary>
 /// <param name="sender">The Frame which failed navigation</param>
 /// <param name="e">Details about the navigation failure</param>
-void App::OnNavigationFailed(IInspectable const&, NavigationFailedEventArgs const& e)
-{
-    throw hresult_error(E_FAIL, hstring(L"Failed to load Page ") + e.SourcePageType().Name);
+void App::OnNavigationFailed(IInspectable const&, NavigationFailedEventArgs const& e) {
+	throw hresult_error(E_FAIL, hstring(L"Failed to load Page ") + e.SourcePageType().Name);
 }
--- UltimateTicTacToe-GUI/App.h
+++ UltimateTicTacToe-GUI/App.h
@@ -1,14 +1,12 @@
 ﻿#pragma once
 #include "App.xaml.g.h"
 
-namespace winrt::UltimateTicTacToe_GUI::implementation
-{
-    struct App : AppT<App>
-    {
-        App();
+namespace winrt::UltimateTicTacToe_GUI::implementation {
+	struct App : AppT<App> {
+		App();
 
-        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
-        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
-        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
-    };
+		void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+		void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+		void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+	};
 }
